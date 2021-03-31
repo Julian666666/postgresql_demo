9449cb2ba048fa79aea0d47f7b63e61129dbb02e
zhufusheng <zhufusheng@lingtu.com>
2021-03-31 09:18:59 +0800

    mapbox

--- src/main/resources/static/js/mapbox_index.js
+++ src/main/resources/static/js/mapbox_index.js
@@ -6,14 +6,32 @@
  * @Version 1.0
  */
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicGFuamlhbndlbiIsImEiOiJjamh2Y2QyOHcweHMzM3F0YzZxbXp2MHZxIn0.GXyyIbdELQWVH93B27FbnQ';
-var tileset = 'mapbox.streets';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2VwdCIsImEiOiJja21weDgwM3YwdWNjMm9ueHl1bG1jYnR4In0.xuN53dn7LWONSlIY108NFQ';
 var map = new mapboxgl.Map({
     container: 'map',
-    zoom: 15,
+    zoom: 17,
     center: [116.81943,32.67051],
-    style: 'mapbox://styles/mapbox/streets-v9',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    preserveDrawingBuffer:true //canvas toDataURL
 });
+
+let nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
+// let geolocate = new mapboxgl.GeolocateControl({
+//     positionOptions: {
+//         // 高精度定位
+//         enableHighAccuracy: true
+//     },
+//     // 跟踪位置变化
+//     trackUserLocation: true
+// });
+// map.addControl(geolocate);
+// 比例尺
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 100,
+    unit: 'metric'
+});
+map.addControl(scale);
 
 map.on('load', function loaded() {
     map.addSource('polygon', {
@@ -29,8 +47,114 @@ map.on('load', function loaded() {
             "visibility":'visible'
         },
         "paint": {
-            "fill-color": "#088",
+            "fill-color": "#468838",
             "fill-opacity": 0.8,
         }
+    });
+});
+layui.use(['layer', 'form'], function () {
+    let layer = layui.layer, form = layui.form;
+    // map.on('dblclick',function (e) {
+    //     // console.log('A dblclick event has occurred at ' + e.lngLat);
+    //     let data = map.getCanvas().toDataURL('image/png');
+    //     console.log(map.getCanvasContainer());
+    //     $('#printImg').attr('src', data);
+    //     layer.open({
+    //         type: 1,
+    //         title: false,
+    //         content: $('#print'),
+    //         area: ['80%', '69%'],
+    //         anim: 0, //0-6的动画形式，-1不开启
+    //         closeBtn: 0,
+    //         shadeClose: true,
+    //         success: function(layero, index){
+    //             // var scale = document.getElementsByClassName('leaflet-control-scale-line')[0].innerHTML;
+    //             // $('#scaleSpan').text('1:' + scale);
+    //         },
+    //         end: function () {
+    //         }
+    //     });
+    // });
+
+    const mapPrint  = new MapPrintTool({
+        enableImg: false, //是否出图
+        fileName: 'mapDemo.png' // 文件名
+    });
+    map.addControl(mapPrint, 'top-left');
+
+    let snapshot = document.getElementById('snapshot');
+    let printImg = $('<img>');
+    // 监听打印 获取数据
+    mapPrint.on('success',function (res) {
+        const data = map.getCanvas().toDataURL('image/png');
+        let mapImg = $('<img>');
+        mapImg.attr('src', data);
+        var w = 708;//printImg.width();//设置最大宽度,也可根据img的外部容器 而动态获得,比如：$("#demo").width();
+        var img_w = $('#map').width();//图片宽度
+        var img_h = $('#map').height();//图片高度
+        if (img_w > w) {//如果图片宽度超出指定最大宽度
+            var height = (w * img_h) / img_w; //高度等比缩放
+            mapImg.css({
+                "width" : w,"height" : height
+            });//设置缩放后的宽度和高度
+        }
+        mapImg.css('border', '2px solid black');
+
+        $(snapshot).empty();
+        $(snapshot).append('<span style="padding-top: 10px; font-size: 25px; color: black;">中国地图</span>');
+        let printDiv = $('<div style="border: 3px solid black; padding: 15px 15px; margin-top: 10px;"></div>');
+        printDiv.append(mapImg);
+        $(snapshot).append(printDiv);
+
+        layer.open({
+            type: 1,
+            title: false,
+            content: $('#print'),
+            area: ['80%', '69%'],
+            anim: 0, //0-6的动画形式，-1不开启
+            closeBtn: 0,
+            shadeClose: true,
+            success: function(layero, index){
+                // var scale = document.getElementsByClassName('leaflet-control-scale-line')[0].innerHTML;
+                // $('#scaleSpan').text('1:' + scale);let printImg = $('<img>');
+                html2canvas(snapshot).then(function(canvas) {
+                    // printImg.src= canvas.toDataURL();
+                    printImg.attr('src', canvas.toDataURL());
+                });
+
+            },
+            end: function () {
+
+            }
+        });
+    });
+
+    form.on('submit(print)', function(data){
+        // IE打印
+        // document.getElementById('WebBrowser').ExecWB(6,2)
+
+        // jqprint
+        // $('#snapshot').jqprint({
+        //     debug: false,
+        //     importCSS: true,
+        //     printContainer: true
+        // });
+
+        printImg.print({
+            //Use Global styles
+            globalStyles : false,
+            //Add link with attrbute media=print
+            mediaPrint : false,
+            //Print in a hidden iframe
+            iframe : false,
+            //Don't print this
+            noPrintSelector : ".avoid-this",
+            //Add this at top
+            // prepend : "中国地图<br/>",
+            //Add this on bottom
+            // append : "<br/>Buh Bye!",
+            //Log to console when printing is done via a deffered callback
+            deferred: $.Deferred().done(function() { console.log('Printing done', arguments); })
+        });
     });
 });
