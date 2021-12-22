6ae6d362637276aa28b1fd853738e759d0d4e407
zhufusheng <zhufusheng@lingtu.com>
2021-12-22 14:13:38 +0800

    port

--- src/main/resources/static/js/mapbox_index.js
+++ src/main/resources/static/js/mapbox_index.js
@@ -36,7 +36,7 @@ map.addControl(scale);
 map.on('load', function loaded() {
     map.addSource('polygon', {
         'type': 'vector',
-        'tiles': ['http://localhost:8081/getTiles/polygon/{z}/{x}/{y}']
+        'tiles': ['http://8.141.156.76:8081/getTiles/polygon/{z}/{x}/{y}']
     });
     map.addLayer({
         'id': 'polygon',
